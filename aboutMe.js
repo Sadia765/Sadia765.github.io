dbecc1fc93b792a60f834e66a8fdc9bce34c0b8c
Sadia Ansari <sadia765@gmail.com>
2020-10-01 20:29:31 -0500

    navbar works

--- aboutMe.js
+++ aboutMe.js
@@ -2,8 +2,9 @@
 toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
+  console.log("inside function");
 }
-
+console.log("outside");
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (e) {
   if (!e.target.matches(".dropbtn")) {
